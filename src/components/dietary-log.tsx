bd754eed6f5477fe5460b1a432f800f479d0942c
Megah Juliardi Sondara Wicaksana <juliardimegah@gmail.com>
2026-01-26 04:03:37 +0000

    i still cant click the drop down, it's seems like disabled and when i cl

--- src/components/dietary-log.tsx
+++ src/components/dietary-log.tsx
@@ -270,7 +270,12 @@ function FoodSearchCombobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0">
+      <PopoverContent
+        className="w-[300px] p-0"
+        onInteractOutside={(e) => {
+          e.preventDefault();
+        }}
+      >
         <Command>
           <CommandInput placeholder="Search food..." />
           <CommandList>
